c0a319de08c5c76126f344af7ed9dea817ad1592
gavinmoore2121 <76663367+gavinmoore2121@users.noreply.github.com>
2021-07-20 15:08:47 -0400

    Updated Mappings

--- src/main/java/com/revature/web/MappingService.java
+++ src/main/java/com/revature/web/MappingService.java
@@ -3,6 +3,7 @@ package com.revature.web;
 import com.revature.daos.PinDAO;
 import com.revature.daos.UserDAO;
 import com.revature.entities.LoginForm;
+import com.revature.entities.Pin;
 import com.revature.entities.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,11 +11,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 //TODO this class will map all necessary functions to Spring services and servlets.
 
 @Controller
 @RequestMapping("mapping")
 public class MappingService implements PinDAO, UserDAO {
+
+    @RequestMapping(value= "/testConnection", method= RequestMethod.GET, produces= MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public ResponseEntity<String> checkPage(@RequestParam(value = "id", defaultValue = "1") Integer id) {
+        return new ResponseEntity<>("Connection valid, here's a number: " + id + "!", HttpStatus.OK);
+    }
 
     /**
      * Validate a user's login, and return the user with the given email or a not-found message.
@@ -32,10 +41,45 @@ public class MappingService implements PinDAO, UserDAO {
         else return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
     }
 
-    @GetMapping("/testConnection")
+
+    @RequestMapping(value= "/getUserPins", method= RequestMethod.POST, produces= MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity<String> checkPage(@RequestParam(value = "id", defaultValue = "1") Integer id) {
-        return new ResponseEntity<>("Connection valid, here's a number: " + id + "!", HttpStatus.OK);
+    public ResponseEntity<List<Pin>> getUserPins(@RequestBody User user) {
+        return new ResponseEntity<List<Pin>>(user.getUserPins(), HttpStatus.OK);
     }
+
+
+    @RequestMapping(value = "/updateUser", method= RequestMethod.PUT)
+    @ResponseBody
+    public ResponseEntity updateUser(@RequestBody User user) {
+        UserDAO.updateUser(user);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @RequestMapping(value = "/createUser", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity createUser(@RequestBody User user) {
+        UserDAO.saveUser(user);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @RequestMapping(value = "/createPin", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity createPin(@RequestBody Pin pin) {
+        PinDAO.savePin(pin);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    // This one might trigger foreign key constaints and fail, will be tested later.
+    @RequestMapping(value = "/deletePin", method = RequestMethod.DELETE)
+    @ResponseBody
+    public ResponseEntity deletePin(@RequestBody Pin pin) {
+        PinDAO.deletePin(pin);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
